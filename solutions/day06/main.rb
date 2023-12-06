ef9e615e65551bfe2ee8fa7adc94f3dbc34366e3
krzaq <krzaq@krzaq.cc>
2023-12-06 06:44:51 +0100

    Optimize Day 06.

--- solutions/day06/main.rb
+++ solutions/day06/main.rb
@@ -4,8 +4,19 @@ DATA = File.read('data.txt').lines
 TS, DS = DATA.map{ _1.scan(/\d+/).map(&:to_i) }
 RACES = [TS, DS].transpose
 
+# def comp_one record, dist
+#     0.upto(record).map{ (record - _1) * _1 }.select{ _1 > dist }.count
+# end
+
+# n * record - n^2 > dist
+# n^2 - n * record + dist < 0
+# a = 1, b = -record, c = dist
+# n = (-b +- sqrt(b^2 - 4ac)) / 2a
 def comp_one record, dist
-    0.upto(record).map{ (record - _1) * _1 }.select{ _1 > dist }.count
+    delta = record ** 2 - 4 * dist
+    x1 = (-record - Math.sqrt(delta)) / 2
+    x2 = (-record + Math.sqrt(delta)) / 2
+    x2.floor - x1.ceil + 1
 end
 
 PART1 = RACES.map{ comp_one *_1 }.inject(:*)
