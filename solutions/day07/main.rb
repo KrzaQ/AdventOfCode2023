11bad4f7d516cf36b71f620799932434dc0c50fe
krzaq <krzaq@krzaq.cc>
2023-12-07 07:01:48 +0100

    Golf Day 07 a little.

--- solutions/day07/main.rb
+++ solutions/day07/main.rb
@@ -1,47 +1,25 @@
 #!/usr/bin/ruby
 
-DATA = File.read('data.txt').lines.map do |line|
-    a, b = line.split
-    [a, b.to_i]
-end
+DATA = File.read('data.txt').lines.map(&:split).map{ [_1, _2.to_i] }
 ORDER_P1 = %w(A K Q J T 9 8 7 6 5 4 3 2)
 ORDER_P2 = %w(A K Q T 9 8 7 6 5 4 3 2 J)
 
 def hand_strength hand
-    case hand.chars.sort.group_by(&:itself).map{ _2.count }.sort
-    when [1, 1, 1, 1, 1]
-        1
-    when [1, 1, 1, 2]
-        2
-    when [1, 2, 2]
-        3
-    when [1, 1, 3]
-        4
-    when [2, 3]
-        5
-    when [1, 4]
-        6
-    when [5]
-        7
-    end
+    type = hand.chars.group_by(&:itself).map{ _2.count }.sort.join
+    %w(11111 1112 122 113 23 14 5).index type
 end
-
 
 def hand_strength_p2 hand
     ORDER_P2.map{ hand_strength hand.gsub(?J, _1) }.max
 end
 
 def compare_hands a, b, mode = :part1
-    aa, bb, order = if mode == :part1
-        [a, b].map{ hand_strength _1 } << ORDER_P1
-    else
-        [a, b].map{ hand_strength_p2 _1 } << ORDER_P2
-    end
-    if aa == bb
-        [b, a].map{ _1.chars.map{ |el| order.index el } }.inject(:<=>)
-    else
-        aa <=> bb
-    end
+    aa, bb, order = {
+        part1: -> { [hand_strength(a),    hand_strength(b),    ORDER_P1] },
+        part2: -> { [hand_strength_p2(a), hand_strength_p2(b), ORDER_P2] },
+    }[mode][]
+    ifsame = -> x { x.map{ _1.chars.map{ |el| order.index el } }.inject(:<=>) }
+    aa == bb ? ifsame[[b, a]] : aa <=> bb
 end
 
 def compute part
@@ -49,7 +27,7 @@ def compute part
         compare_hands a[0], b[0], part
     end.map.with_index do |el, i|
         hand, score = el
-        score * (i + 1)
+        score * i.succ
     end.sum
 end
 
