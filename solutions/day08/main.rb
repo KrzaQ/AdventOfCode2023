2c179f87cf665385438011b65c50039e1c3653b5
krzaq <krzaq@krzaq.cc>
2023-12-08 07:42:09 +0100

    Golf Day 08 a little.

--- solutions/day08/main.rb
+++ solutions/day08/main.rb
@@ -1,18 +1,12 @@
 #!/usr/bin/ruby
 
 DATA = File.read('data.txt').lines
-
 INSTRUCTIONS = DATA.first.strip
 PATH = DATA[2..-1].map{ a,b,c = _1.scan(/[\w\d]{3}/); [a, [b, c]] }.to_h
 
 def walk_path x, rx = /^ZZZ$/
     (0..).each do |i|
-        lr = INSTRUCTIONS[i % INSTRUCTIONS.size]
-        x = if lr == ?L
-            PATH[x][0]
-        elsif lr == ?R
-            PATH[x][1]
-        end
+        x = PATH[x][INSTRUCTIONS[i % INSTRUCTIONS.size] == ?L ? 0 : 1]
         return i.succ if x =~ rx
     end
 end
